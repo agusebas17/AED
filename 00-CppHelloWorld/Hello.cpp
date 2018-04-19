08da9c750c84957d57e1572d56c1d78e6ba3a065
agusebas17 <agusebas17@gmail.com>
2018-04-19 13:45:50 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/Hello.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Agustin Sebastian
+// Version     :1.0
+// Copyright   : Your copyright notice
+
+//============================================================================
+//
+//
+
+#include <iostream>
+
+int main() {
+	std::cout << "!!!Hello World!!!\n" ; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ 00-CppHelloWorld/README.md
@@ -0,0 +1,4 @@
+# CppHelloWorld
+
+## Trabajo Practico 0
+Agustin Sebastian
